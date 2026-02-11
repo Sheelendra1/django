c6e48ec17684c9e56cc837448509ec5655a426fe
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-02-11 14:19:23 +0530

    password hashing

--- SQLalchemy/app.py
+++ SQLalchemy/app.py
@@ -1,19 +1,43 @@
-from flask import Flask,request,render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'secret_key'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Welcome to home"
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route("/form", methods=['GET', 'POST'])
-def form():
-    if request.method=='POST':
-        username=request.form.get('username')
-        email=request.form.get('email')
+        if User.query.filter_by(username=username).first():
+            flash("User already exists")
+            return redirect(url_for('home'))
 
-        return f"welcome {username} | your email {email}"
-    
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("User registered successfully!")
+        return redirect(url_for('home'))
+
     return render_template('form.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- SQLalchemy/login/app.py
+++ SQLalchemy/login/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 db = SQLAlchemy(app)
