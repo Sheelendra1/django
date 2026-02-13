b412d4dc2bc795c7148ef77493835413f55acdaf
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-02-13 20:41:51 +0530

    showing details and images from backned to frontend

--- django/New_Django/app/urls.py
+++ django/New_Django/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('details/', views.student_details, name='student_details'),
 ]
--- django/New_Django/app/views.py
+++ django/New_Django/app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 
 def home(request):
     return render(request, 'app.html')
+
+def student_details(request):
+    students = Student.objects.all()
+    return render(request, 'student_details.html', {'students': students})
