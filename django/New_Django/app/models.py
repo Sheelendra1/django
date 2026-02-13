c7cfce42e65a8c590d312d09bf059fba6026f52d
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-02-13 11:11:12 +0530

    super admin create

--- django/New_Django/app/models.py
+++ django/New_Django/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    BRANCH_CHOICES = [
+        ('CS', 'Computer Science'),
+        ('ELEC', 'Electronics'),
+        ('CIVIL', 'Civil Engineering'),    
+    ]
+
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    age=models.IntegerField()
+    branch = models.CharField(max_length=20, choices=BRANCH_CHOICES)
+
+    def __str__(self):
+        return self.name
