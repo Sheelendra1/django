ad302d9f48fdc3f89eaf8e518916660dbf5cd8c7
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-02-17 13:43:25 +0530

    role based login

--- django/djangoform/app/views.py
+++ django/djangoform/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import UserForm
+
+def user_form_view(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = UserForm()
+
+    return render(request, 'form.html', {'form': form})
